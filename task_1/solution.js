d916ba31ba07d5e79eb9cd9356576e48b4c31578
Alla <allaungakova@gmail.com>
2023-06-05 17:38:44 +0300

    tack_1 done_2

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,14 +1,14 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    
+
+    // Задание №1.1. Оповещение о добавлении в корзину
     let message;
 
     message = productName + " за " + productPrice + " теперь в корзине!";
     
     console.log(message);
-
-
-    // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
     // присвойте переменной message значение переменной productName
@@ -29,6 +29,7 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -44,6 +45,9 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+    let newSum;
+    newSum = oldSum + difference;
+    let newSumText = `${newSum} р`;
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
